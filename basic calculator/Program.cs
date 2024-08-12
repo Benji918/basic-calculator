4b9edc471fa819d19b5b0990331ff6212dc40732
Benjamin <kodiugos@gmail.com>
2024-08-11 23:22:05 -0500

    Add project files.

---
+++ basic calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+// TODO: Implement the calculator logic here
+
+//Prompt user to enter first number
+Console.WriteLine("Enter the first number");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+//Prompt user to enter second number
+Console.WriteLine("Enter the second number");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+//Promot user to enter arithmetic operator
+Console.WriteLine("Choose an operation: +, -, *, /");
+string arithmetic_op = Console.ReadLine();
+
+if (arithmetic_op == "+")
+{
+    Console.WriteLine($"Result {num1 + num2}");
+}
+else if (arithmetic_op == "-")
+{
+    Console.WriteLine($"Result {num1 - num2}");
+}
+else if (arithmetic_op == "/")
+{
+    if (num2 != 0)
+    {
+        Console.WriteLine($"Result {num1 / num2}");
+    }
+    else
+    {
+        Console.WriteLine("Error: Division by zero is not allowed.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
+}
+
+
+Console.ReadKey();
